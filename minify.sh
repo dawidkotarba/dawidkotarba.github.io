32200bf039482d537093f13018843e617547bb72
dawidkotarba <dawidkotarba>
2020-02-28 15:36:59 +0100

    Minify js

--- minify.sh
+++ minify.sh
@@ -6,3 +6,9 @@ CSS_FOLDER=css
 yui-compressor ${CSS_FOLDER}/fonts.css > ${CSS_FOLDER}/fonts.min.css
 yui-compressor ${CSS_FOLDER}/ie8.css > ${CSS_FOLDER}/ie8.min.css
 yui-compressor ${CSS_FOLDER}/style.css > ${CSS_FOLDER}/style.min.css
+
+echo "Processing JS..."
+JS_FOLDER=js
+yui-compressor ${JS_FOLDER}/global.js > ${JS_FOLDER}/global.min.js
+yui-compressor ${JS_FOLDER}/html5.js > ${JS_FOLDER}/html5.min.js
+yui-compressor ${JS_FOLDER}/jquery.scrollTo.js > ${JS_FOLDER}/jquery.scrollTo.min.js
