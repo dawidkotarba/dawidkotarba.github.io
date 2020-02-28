1f2e5cf9f28f99f3e9eb6b8d32c4ca468f212927
dawidkotarba <dawidkotarba>
2020-02-28 15:41:20 +0100

    Add custom.js and custom.css

--- minify.sh
+++ minify.sh
@@ -6,9 +6,11 @@ CSS_FOLDER=css
 yui-compressor ${CSS_FOLDER}/fonts.css > ${CSS_FOLDER}/fonts.min.css
 yui-compressor ${CSS_FOLDER}/ie8.css > ${CSS_FOLDER}/ie8.min.css
 yui-compressor ${CSS_FOLDER}/style.css > ${CSS_FOLDER}/style.min.css
+yui-compressor ${CSS_FOLDER}/custom.css > ${CSS_FOLDER}/custom.min.css
 
 echo "Processing JS..."
 JS_FOLDER=js
 yui-compressor ${JS_FOLDER}/global.js > ${JS_FOLDER}/global.min.js
 yui-compressor ${JS_FOLDER}/html5.js > ${JS_FOLDER}/html5.min.js
 yui-compressor ${JS_FOLDER}/jquery.scrollTo.js > ${JS_FOLDER}/jquery.scrollTo.min.js
+yui-compressor ${JS_FOLDER}/custom.js > ${JS_FOLDER}/custom.min.js
