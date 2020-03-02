90c91cbbdecba7151f7c9c78a7699f92e445eea0
dawidkotarba <dawidkotarba>
2020-03-02 22:38:35 +0100

    Install GA tags manager

--- minify.sh
+++ minify.sh
@@ -22,4 +22,4 @@ echo "Processing CSS..."
 minifyCss fonts ie8 style donuts custom
 printf "\n"
 echo "Processing JS..."
-minifyJs global html5 jquery.scrollTo custom ga
+minifyJs global html5 jquery.scrollTo custom ga ga-tags
