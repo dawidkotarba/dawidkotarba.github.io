92e87071424055a0873c121515c6eb990d846f33
dawidkotarba <dawidkotarba>
2020-03-05 13:15:52 +0100

    Add gulp watch task, move bundles to bundle folders

--- gulpfile.js
+++ gulpfile.js
@@ -9,33 +9,34 @@ gulp.task('js', function () {
     gulp.src(['js/jquery.js', 'js/jquery-migrate.min.js', 'js/jquery.scrollTo.js', 'js/global.js', 'js/ga-tags.js', 'js/navigation.js', 'js/custom.js'])
         .pipe(concat('bundle.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('js/'));
+        .pipe(gulp.dest('js/dist'));
 
     gulp.src('js/html5.js')
         .pipe(concat('html5.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('js/'));
+        .pipe(gulp.dest('js/dist'));
 
     return gulp.src('js/ga.js')
         .pipe(concat('ga.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('js/'))
+        .pipe(gulp.dest('js/dist'))
 });
 
 gulp.task('css', function () {
     gulp.src(['css/style.css', 'css/ie8.css', 'css/fonts.css', 'css/donuts.css', 'css/animate.min.css', 'css/custom.css'])
         .pipe(concat('bundle.css'))
         .pipe(minifyCss())
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('css/dist'));
 
     return gulp.src('css/ie8.css')
         .pipe(concat('ie8.min.css'))
         .pipe(minifyCss())
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('css/dist'));
 });
 
 gulp.task('html', function () {
     return gulp.src(['index_dev.html'])
+
         .pipe(minifyHtml({
             collapseWhitespace: true,
             removeComments: true
@@ -43,5 +44,12 @@ gulp.task('html', function () {
         .pipe(rename({basename: 'index'}))
         .pipe(gulp.dest('./'));
 });
+
+gulp.task('watch', function () {
+    gulp.watch('./**/js/*.js', gulp.series('js'));
+    gulp.watch('./**/css/*.css', gulp.series('css'));
+    gulp.watch('./**/index_dev.html', gulp.series('html'));
+});
+
 
 gulp.task('default', gulp.series('js', 'css', 'html'));
