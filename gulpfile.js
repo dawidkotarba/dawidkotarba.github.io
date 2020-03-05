42b4f3ccc3ec52cc7d547e56e526470e6fc9aee2
dawidkotarba <dawidkotarba>
2020-03-05 12:51:48 +0100

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,47 +5,43 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var minifyHtml = require('gulp-htmlmin');
 
-gulp.task('js', (done) => {
-    gulp.src(['./js/jquery.js', './js/jquery-migrate.min.js', './js/jquery.scrollTo.js', './js/global.js', './js/ga-tags.js','./js/navigation.js',  './js/custom.js'])
+gulp.task('js', function () {
+    gulp.src(['js/jquery.js', 'js/jquery-migrate.min.js', 'js/jquery.scrollTo.js', 'js/global.js', 'js/ga-tags.js', 'js/navigation.js', 'js/custom.js'])
         .pipe(concat('bundle.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./js/'));
+        .pipe(gulp.dest('js/'));
 
-    gulp.src('./js/html5.js')
+    gulp.src('js/html5.js')
         .pipe(concat('html5.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./js/'));
+        .pipe(gulp.dest('js/'));
 
-    gulp.src('./js/ga.js')
+    return gulp.src('js/ga.js')
         .pipe(concat('ga.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./js/'))
-
-    done();
+        .pipe(gulp.dest('js/'))
 });
 
-gulp.task('css', (done) => {
-    gulp.src(['./css/style.css', './css/ie8.css', './css/fonts.css', './css/donuts.css', './css/animate.min.css', './css/custom.css'])
+gulp.task('css', function () {
+    gulp.src(['css/style.css', 'css/ie8.css', 'css/fonts.css', 'css/donuts.css', 'css/animate.min.css', 'css/custom.css'])
         .pipe(concat('bundle.css'))
         .pipe(minifyCss())
-        .pipe(gulp.dest('./css/'));
+        .pipe(gulp.dest('css/'));
 
-    gulp.src('./css/ie8.css')
+    return gulp.src('css/ie8.css')
         .pipe(concat('ie8.min.css'))
         .pipe(minifyCss())
-        .pipe(gulp.dest('./css/'));
-    done();
+        .pipe(gulp.dest('css/'));
 });
 
-gulp.task('html', (done) => {
-    gulp.src(['./index_dev.html'])
+gulp.task('html', function () {
+    return gulp.src(['index_dev.html'])
         .pipe(minifyHtml({
             collapseWhitespace: true,
             removeComments: true
         }))
-        .pipe(rename({basename:'index'}))
+        .pipe(rename({basename: 'index'}))
         .pipe(gulp.dest('./'));
-    done();
 });
 
 gulp.task('default', gulp.series('js', 'css', 'html'));
