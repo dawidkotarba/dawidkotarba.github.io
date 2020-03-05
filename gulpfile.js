6f4eae6bf8dbb2c5634fbba4989ecd6f4b436afe
dawidkotarba <dawidkotarba>
2020-03-05 14:08:48 +0100

    Add browsersync and gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,40 @@ var uglify = require('gulp-uglify');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var minifyHtml = require('gulp-htmlmin');
+var browserSync = require('browser-sync').create();
 
 gulp.task('js', function () {
     gulp.src(['js/jquery.js', 'js/jquery-migrate.min.js', 'js/jquery.scrollTo.js', 'js/global.js', 'js/ga-tags.js', 'js/navigation.js', 'js/custom.js'])
         .pipe(concat('bundle.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('js/dist'));
+        .pipe(gulp.dest('js/dist'))
+        .pipe(browserSync.stream());
 
     gulp.src('js/html5.js')
         .pipe(concat('html5.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('js/dist'));
+        .pipe(gulp.dest('js/dist'))
+        .pipe(browserSync.stream());
 
     return gulp.src('js/ga.js')
         .pipe(concat('ga.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('js/dist'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('css', function () {
     gulp.src(['css/style.css', 'css/ie8.css', 'css/fonts.css', 'css/donuts.css', 'css/animate.min.css', 'css/custom.css'])
         .pipe(concat('bundle.css'))
         .pipe(minifyCss())
-        .pipe(gulp.dest('css/dist'));
+        .pipe(gulp.dest('css/dist'))
+        .pipe(browserSync.stream());
 
     return gulp.src('css/ie8.css')
         .pipe(concat('ie8.min.css'))
         .pipe(minifyCss())
-        .pipe(gulp.dest('css/dist'));
+        .pipe(gulp.dest('css/dist'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('html', function () {
@@ -51,5 +57,20 @@ gulp.task('watch', function () {
     gulp.watch('./**/index_dev.html', gulp.series('html'));
 });
 
+gulp.task('browser-sync', function (done) {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 
-gulp.task('default', gulp.series('js', 'css', 'html'));
+    gulp.watch(['./**/index.html'], gulp.series(function reload(done) {
+        browserSync.reload();
+        done();
+    }));
+    done();
+});
+
+gulp.task('serve', gulp.parallel('watch', 'browser-sync'));
+
+gulp.task('default', gulp.parallel('serve'));
