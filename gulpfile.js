6b1987aaf9e3e6466757fbab8358e98035dc3d08
dawidkotarba <dawidkotarba>
2020-03-05 21:29:13 +0100

    Update gulp config - add img processing, results are now in dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,26 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var minifyHtml = require('gulp-htmlmin');
 var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
 
 gulp.task('js', function () {
     gulp.src(['js/jquery.js', 'js/jquery-migrate.min.js', 'js/jquery.scrollTo.js', 'js/global.js', 'js/ga-tags.js', 'js/navigation.js', 'js/custom.js'])
         .pipe(concat('bundle.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('js/dist'))
+        .pipe(gulp.dest('dist/js'))
         .pipe(browserSync.stream());
 
     gulp.src('js/html5.js')
         .pipe(concat('html5.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('js/dist'))
+        .pipe(gulp.dest('dist/js'))
         .pipe(browserSync.stream());
 
     return gulp.src('js/ga.js')
         .pipe(concat('ga.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('js/dist'))
+        .pipe(gulp.dest('dist/js'))
         .pipe(browserSync.stream());
 });
 
@@ -30,13 +32,13 @@ gulp.task('css', function () {
     gulp.src(['css/style.css', 'css/ie8.css', 'css/fonts.css', 'css/donuts.css', 'css/animate.min.css', 'css/custom.css'])
         .pipe(concat('bundle.css'))
         .pipe(minifyCss())
-        .pipe(gulp.dest('css/dist'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
 
     return gulp.src('css/ie8.css')
         .pipe(concat('ie8.min.css'))
         .pipe(minifyCss())
-        .pipe(gulp.dest('css/dist'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
 });
 
@@ -51,9 +53,16 @@ gulp.task('html', function () {
         .pipe(gulp.dest('./'));
 });
 
+gulp.task('img', function () {
+    return gulp.src('img/**/*.+(png|jpg|gif|svg)')
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest('dist/img'))
+});
+
 gulp.task('watch', function () {
     gulp.watch('./**/js/*.js', gulp.series('js'));
     gulp.watch('./**/css/*.css', gulp.series('css'));
+    gulp.watch('./**/img/*.*', gulp.series('img'));
     gulp.watch('./**/index_dev.html', gulp.series('html'));
 });
 
@@ -72,5 +81,6 @@ gulp.task('browser-sync', function (done) {
 });
 
 gulp.task('serve', gulp.parallel('watch', 'browser-sync'));
+gulp.task('build', gulp.parallel('js', 'css', 'img', 'html'));
 
 gulp.task('default', gulp.parallel('serve'));
