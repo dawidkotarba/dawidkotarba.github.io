064faa0366500c857db4bb580facec9499f83cb8
dawidkotarba <dawidkotarba>
2020-03-07 12:04:13 +0100

    Update tests

--- cypress/integration/general_spec.js
+++ cypress/integration/general_spec.js
@@ -11,6 +11,19 @@ describe('General tests', function () {
     it('Homepage menu button should navigate to dawidkotarba.github.io', function () {
         cy.get('#link-homepage')
             .should('have.prop', 'href')
-            .and('equal', 'https://dawidkotarba.github.io/')
+            .and('contains', 'https://dawidkotarba.github.io')
     });
+
+    it('LinkedIn menu button should navigate to LinkedIn profile', function () {
+        cy.get('#link-linkedin')
+            .should('have.prop', 'href')
+            .and('equal', 'https://www.linkedin.com/in/dawid-kotarba-425306a5')
+    });
+
+    it('GitHub menu button should navigate to GitHub page', function () {
+        cy.get('#link-github')
+            .should('have.prop', 'href')
+            .and('equal', 'https://github.com/dawidkotarba')
+    });
+
 });
