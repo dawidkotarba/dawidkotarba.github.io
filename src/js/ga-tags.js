af684cdc4ba4553619c4bd47bb40334e19b0f7b0
dawidkotarba <dawidkotarba>
2020-03-15 12:42:26 +0100

    Disable GA for dev/test envs

--- src/js/ga-tags.js
+++ src/js/ga-tags.js
@@ -1,13 +1,15 @@
-(function (w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({
-        'gtm.start':
-            new Date().getTime(), event: 'gtm.js'
-    });
-    var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-    j.async = true;
-    j.src =
-        'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-    f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-P6L68NW');
+if (document.location.hostname.search("dawidkotarba.github.io") !== -1) {
+    (function (w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start':
+                new Date().getTime(), event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-P6L68NW');
+}
--- src/js/ga.js
+++ src/js/ga.js
@@ -1,8 +1,10 @@
-window.dataLayer = window.dataLayer || [];
+if (document.location.hostname.search("dawidkotarba.github.io") !== -1) {
+    window.dataLayer = window.dataLayer || [];
 
-function gtag() {
-    dataLayer.push(arguments);
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+    gtag('config', 'UA-159312468-1');
 }
-
-gtag('js', new Date());
-gtag('config', 'UA-159312468-1');
