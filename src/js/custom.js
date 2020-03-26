bba213982f0bf6b3028af238921ff1d84be5cc51
dawidkotarba <dawidkotarba>
2020-03-26 10:09:35 +0100

    Add toggling of comm exp

--- src/js/custom.js
+++ src/js/custom.js
@@ -6,3 +6,8 @@ var observer = lozad('.lozad', {
     rootMargin: '500px 0px'
 });
 observer.observe();
+
+// toggle commercial experience
+$('.employment-card').click(function(){
+    $(this).toggleClass('employment-card-clicked');
+});
