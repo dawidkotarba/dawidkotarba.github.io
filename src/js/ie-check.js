30b5886362002895b4ddf779079f3974fdeee693
dawidkotarba <dawidkotarba>
2020-03-29 00:54:22 +0100

    Update popup for IE

--- src/js/ie-check.js
+++ src/js/ie-check.js
@@ -4,4 +4,6 @@ if (/(MSIE\ [0-9]{1})/i.test(navigator.userAgent) || /(Trident\/[7]{1})/i.test(n
     ieCheckDiv.innerHTML = 'Internet Explorer is not supported and the site may be (and for sure is) broken.';
     ieCheckDiv.setAttribute("style", "background-color:#000;color:#FFF;text-align:center;padding:10px;");
     document.body.insertBefore(ieCheckDiv, document.body.firstChild);
+    // remove the header image as it hides the label once it appears
+    document.getElementById('header').innerHTML = '';
 }
