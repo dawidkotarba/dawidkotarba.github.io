570a2ef89570062fe811b936456c5b28e634ccf5
dawidkotarba <dawidkotarba>
2020-04-13 18:49:45 +0200

    Lazily load images after the page is loaded for a better performance

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,13 +1,22 @@
+// toggle commercial experience
+$('.employment-card').click(function () {
+    $(this).toggleClass('employment-card-clicked');
+});
+
 // aos
 AOS.init();
 
 // lazy loading
-var observer = lozad('.lozad', {
+var lozadClass = '.lozad';
+
+// load images 1000px before it is visible
+var observer = lozad(lozadClass, {
     rootMargin: '1000px 0px'
 });
 observer.observe();
 
-// toggle commercial experience
-$('.employment-card').click(function(){
-    $(this).toggleClass('employment-card-clicked');
+// load images after page is rendered
+// for a better performance
+$(lozadClass).each(function () {
+    observer.triggerLoad(this);
 });
