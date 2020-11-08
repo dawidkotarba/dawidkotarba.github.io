0bd4f7708447baad8d58644a645085a6637e26cb
dawidkotarba <dawidkotarba>
2020-11-08 12:17:02 +0100

    Add a button to a cookie popup

--- src/js/cookie-popup.js
+++ src/js/cookie-popup.js
@@ -3,7 +3,7 @@
         document.body.innerHTML += '\
 		<div class="cookieconsent animated fadeInUp" style="position:fixed;padding:10px;left:0;bottom:0;background-color:#000;color:#FFF;text-align:center;width:100%;z-index:99999; font-family: \'Libre Franklin\', \'Helvetica Neue\', Helvetica, Arial, sans-serif;">\
 			This site uses cookies. By continuing to use this website, you agree to their use. \
-			<a href="#" style="color:#CCCCCC;">OK! I like cookies:)</a>\
+		    <div><a href="#" class="cookieBtn">OK! I like cookies:)</a></div>\
 		</div>\
 		';
         document.querySelector('.cookieconsent a').onclick = function (e) {
