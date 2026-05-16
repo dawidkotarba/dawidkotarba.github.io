ca9ecd5876f254ccbae2b24094415bc93b7f05e3
dawidkotarba <dawidkotarba>
2026-05-16 23:37:30 +0200

    Add parallax for title

--- src/js/custom.js
+++ src/js/custom.js
@@ -3,6 +3,23 @@ $('.employment-card').click(function () {
     $(this).toggleClass('employment-card-clicked');
 });
 
+// title parallax effect on scroll
+$(window).scroll(function () {
+    var scrollPos = $(this).scrollTop();
+    var $branding = $('.site-branding');
+
+    // effect happens only in the header area
+    if (scrollPos < 500) {
+        var scale = 1 + (scrollPos / 1000); // slowly scales up
+        var opacity = 1 - (scrollPos / 400); // fades out
+
+        $branding.css({
+            'transform': 'translateY(-50%) scale(' + scale + ')',
+            'opacity': opacity >= 0 ? opacity : 0
+        });
+    }
+});
+
 // aos
 AOS.init();
 
